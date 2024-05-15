3934c786e1082c3e9524f88ed059db743f36c94f
jarvisjiang <jarvisjiang@tencent.com>
2024-05-15 15:28:57 +0800

    add scripts

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_txt;
+
+    private void Awake()
+    {
+        m_txt.text = "Hello Tuanjie!";
+    }
+}
