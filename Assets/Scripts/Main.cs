4f21203ce416ef26cb457f883ad7d7e79a1a9c6b
jarvisjiang <jarvisjiang@tencent.com>
2024-05-18 23:18:52 +0800

    add scripts

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,32 +1,12 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Main : MonoBehaviour
 {
-    private AsyncOperationHandle<GameObject> opHandle;
-
     [SerializeField] private TextMeshProUGUI m_txt;
 
     private void Awake()
     {
-        m_txt.text = "Hello Tuanjie!";
-
-        var key = "Res/Main.prefab";
-        opHandle = Addressables.LoadAssetAsync<GameObject>(key);
-        opHandle.Completed += (op) =>
-        {
-            if (opHandle.Status == AsyncOperationStatus.Succeeded)
-            {
-                var obj = opHandle.Result;
-                Instantiate(obj, transform);
-            }
-        };
-    }
-
-    private void OnDestroy()
-    {
-        Addressables.Release(opHandle);
+        m_txt.text = "Tuanjie Main!";
     }
 }
