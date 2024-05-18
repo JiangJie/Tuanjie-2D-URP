4920ea10636e69c095378cb26b1305a31344d73e
jarvisjiang <jarvisjiang@tencent.com>
2024-05-18 22:09:37 +0800

    load Main.prefab from addressable

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,12 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Main : MonoBehaviour
 {
+    private AsyncOperationHandle<GameObject> opHandle;
+
     [SerializeField] private TextMeshProUGUI m_txt;
 
     private void Awake()
     {
         m_txt.text = "Hello Tuanjie!";
+
+        var key = "Res/Main.prefab";
+        opHandle = Addressables.LoadAssetAsync<GameObject>(key);
+        opHandle.Completed += (op) =>
+        {
+            if (opHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                var obj = opHandle.Result;
+                Instantiate(obj, transform);
+            }
+        };
+    }
+
+    private void OnDestroy()
+    {
+        Addressables.Release(opHandle);
     }
 }
